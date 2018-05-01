317f4e0abfc8d378089d0fb33ebdda277605d4d3
Juan Miguel Alvarez <juanmi.alvarez@gmail.com>
2018-05-01 18:47:23 +0100

    Message render insert last message as first

--- client/src/main/scala/com/github/jalvarez/mirafondo/MessageRender.scala
+++ client/src/main/scala/com/github/jalvarez/mirafondo/MessageRender.scala
@@ -6,7 +6,10 @@ import MessageUnpacking.Message
 class MessageRender(elementList: dom.Element) {
   def add(message: Message): Unit = {
     val li = dom.document.createElement("li")
-    li.textContent = message.content
-    elementList.appendChild(li)
+    li.textContent = s"(${message.position}) ${message.content}"
+    if (elementList.childElementCount > 0)
+      elementList.insertBefore(li, elementList.firstChild)
+    else
+      elementList.appendChild(li)
   }
 }
--- client/src/test/scala/com/github/jalvarez/mirafondo/MessageRenderSpec.scala
+++ client/src/test/scala/com/github/jalvarez/mirafondo/MessageRenderSpec.scala
@@ -8,22 +8,33 @@ import MessageUnpacking.Message
 class MessageRenderSpec extends WordSpec with Matchers {
   "The message render" when {
     "a message is added" must {
+      val f = fixtures
+      
+      val render = new MessageRender(f.list)
+      render.add(f.message)
+      
       "add a list item" in {
-        val f = fixtures
-        
-        val render = new MessageRender(f.list)
-        render.add(f.message)
-        
         f.list.childElementCount should be > 0
       }
       
       "include the message's content" in {
-        val f = fixtures
-        
-        val render = new MessageRender(f.list)
-        render.add(f.message)
-        
         f.list.innerHTML should include (f.message.content)
+      }
+      
+      "include the message's position" in {
+        f.list.innerHTML should include (f.message.position.toString)
+      }
+    }
+    
+    "two messages are added" must {
+      val f = fixtures
+      val secondMessage = Message(102, "test-2", 84)
+      val render = new MessageRender(f.list)
+      render.add(f.message)
+      render.add(secondMessage)
+      
+      "include the last message as first" in {
+        f.list.children(0).innerHTML should include (secondMessage.position.toString)
       }
     }
   }
