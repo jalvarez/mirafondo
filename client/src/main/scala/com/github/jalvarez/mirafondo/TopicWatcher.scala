54f2038b0bc88039b08b3702beea574c3a544b26
Juan Miguel <juanmi.alvarez@gmail.com>
2018-05-11 23:21:59 +0100

    Previous messages button works

--- client/src/main/scala/com/github/jalvarez/mirafondo/TopicWatcher.scala
+++ client/src/main/scala/com/github/jalvarez/mirafondo/TopicWatcher.scala
@@ -2,15 +2,41 @@ package com.github.jalvarez.mirafondo
 
 import org.scalajs.dom
 import dom.raw.XMLHttpRequest
+import scala.util.Try
+import shared.Defaults
 
 object TopicWatcher {
   def run(context: String, urlPath: String): Unit = {
-    val topicWatchUrl = TopicWatchPath(urlPath + dom.window.location.search)
+    val topicWatchPath = TopicWatchPath(urlPath + dom.window.location.search)
     val xhr = HttpRequestSimplificator(new XMLHttpRequest())
     val messageList = new MessageList(xhr, context)
     val messageRender = new MessageRender(dom.document.getElementById("items"))
+    var lastPosition: Option[Long] = None
     
-    messageList.setOnNewMessage { message => messageRender.add(message) }
-               .load(topicWatchUrl.topicName, topicWatchUrl.position)
+    messageList.setOnNewMessage { message =>
+                  messageRender.add(message)
+                  if (lastPosition.isEmpty) {
+                    lastPosition = Some(message.position)
+                    val previousPosition = for (pos <- lastPosition;
+                                                newPos <- Some(pos - Defaults.MESSAGES_LIMIT) if newPos > 0)
+                                                  yield newPos
+                    updateButton(topicWatchPath, previousPosition)
+                  }
+                }
+               .load(topicWatchPath.topicName, topicWatchPath.position)
+  }
+  
+  private def updateButton(topicWatchPath: TopicWatchPath, previousPosition: Option[Long]): Unit = {
+    for (button <- Try(dom.document.getElementById("previous-button").asInstanceOf[dom.html.Button])) {
+      previousPosition match {
+        case Some(pos) =>
+          button.style.visibility = "visible"
+          button.onclick = { _ =>
+            dom.window.location.href = topicWatchPath.newPathTo(pos)
+          }
+        case None =>
+          button.style.visibility = "hidden"
+      }
+    }
   }
 }
